f0bae56fe545816efc8909726ab22f4dd6289c1f
Daniel <soulfantasy_kilua@live.com>
2019-07-31 18:48:31 -0500

    Update getProduct.php

--- lib/api/getProduct.php
+++ lib/api/getProduct.php
@@ -6,8 +6,8 @@ set_time_limit(1200);
 
 class getProduct {
     public function Info(){
-        $client = new SoapClient('https://ideac.com.mx/store/api/soap/?wsdl');
-        $session = $client->login('P41N3ST', '78ae61b5c3af8b9630a74d37da1407a4');
+        $client = new SoapClient('https://**.com/store/api/soap/?wsdl');
+        $session = $client->login('**', '**');
         $countedRows = new Counted;
         for($i=1104; $i<$countedRows->rows()+1104; $i++){
             $call = $client->call($session, 'catalog_product.info', '1105');
