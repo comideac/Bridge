e3f8ab3cb187b33955325536f0387322878688cd
P41N3ST <soulfantasy_kilua@live.com>
2019-08-01 18:58:12 -0500

    Done api integration

--- lib/api/getProduct.php
+++ lib/api/getProduct.php
@@ -9,46 +9,63 @@ use PHPMailer\PHPMailer\Exception;
 
 set_time_limit(1200);
 
+header("Content-Type: text/html; charset=utf-8");
+#header('Content-Type: application/json');
+
 class getProduct {
-<<<<<<< HEAD
     public function Id(){
+        $serverName = 'SENDBOXSERVER\\COMPAC2';
+        $serverInfo = array('Database' => 'adCOMERCIALIZADORAIDE');
+        $conn = sqlsrv_connect($serverName, $serverInfo);
+
         $client = new SoapClient('https://ideac.com.mx/store/api/soap/?wsdl');
         $session = $client->login('P41N3ST', '78ae61b5c3af8b9630a74d37da1407a4');
-        $call = $client->call($session, 'catalog_product.list');
-        foreach($call as $id){
-            $id = $id['sku'];
-            $updt = $client->call($session, 'cataloginventory_stock_item.update');
-        }
-    }
-
-    public function Stock(){
         $sheet = IOFactory::load('../inventario.csv');
         $sheet = $sheet->getActiveSheet()->toArray(null, true, true, true);
-        for($i=1;$i<count($sheet)+1;$i++){
-            $list = array($sheet[$i]['B']);
-            if(!empty($list[0])){
-                if($list[0]!="--------------------------------"){
-                    return $list[0];
+        foreach($sheet as $qx){
+            if(!empty($qx['B'])){
+                if($qx['B'] != "--------------------------------"){
+                    if($qx['B'] > 0){
+                        $in_stock = 1;
+                        $attributes = array(
+                            'qty' => $qx['J'],
+                            'is_in_stock' => $in_stock,
+                            'manage_stock' => 1,
+                            'use_config_manage_stock' => 0,
+                            'min_qty' => 1,
+                            'use_config_min_qty' => 0,
+                            'min_sale_qty' => 1,
+                            'use_config_min_sale_qty' => 0,
+                            'max_sale_qty' => 10,
+                            'use_config_max_sale_qty' => 0,
+                            'is_qty_decimal' => 0,
+                            'backorder' => 1,
+                            'use_config_backorders' => 0,
+                            'notify_stock_qty' => 0,
+                            'use_config_notify_stock_qty' => 0
+                        );
+                        $sku = $client->call($session, 'catalog_product.list', $qx['B']);
+                        var_dump($sku['SKU']);
+                        #if(){
+                            #$call = $client->call($session, 'cataloginventory_stock_item.update', array($qx['B'], $attributes));
+                        #} else {
+                            #
+                        #}
+                        var_dump($call);
+                    } else {
+                        #
+                    }
                 }
             }
-=======
-    public function Info(){
-        $client = new SoapClient('https://**.com/store/api/soap/?wsdl');
-        $session = $client->login('**', '**');
-        $countedRows = new Counted;
-        for($i=1104; $i<$countedRows->rows()+1104; $i++){
-            $call = $client->call($session, 'catalog_product.info', '1105');
-            #if($call) {
-            #
-            #}
->>>>>>> f0bae56fe545816efc8909726ab22f4dd6289c1f
         }
+        
+        #$query = sqlsrv_query($conn, "SELECT * FROM dbo.admProductos", array(), array('Scrollable' => SQLSRV_CURSOR_KEYSET));
+        #while($row = sqlsrv_fetch_array($query, SQLSRV_FETCH_ASSOC)){
+        #    $x = json_encode($row['CCODIGOPRODUCTO']);
+        #}
+            
     }
 }
 
 $a = new getProduct;
-<<<<<<< HEAD
-$a->Stock();
-=======
-$a->Info();
->>>>>>> f0bae56fe545816efc8909726ab22f4dd6289c1f
+$a->Id();
